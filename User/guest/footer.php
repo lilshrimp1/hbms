4297bec74392b323e13ecd182110ba6b8ed710e0
NemMos16 <129766207+NemMos16@users.noreply.github.com>
2025-05-16 22:44:23 +0800

    Add files via upload

---
+++ User/guest/footer.php
@@ -0,0 +1,14 @@
+</body>
+
+<footer>
+    <div>
+        <h2>HBMS</h2>
+        <p>Information about the system or the hotel</p>
+    </div>
+
+    <div>
+        <h2>feedback</h2>
+    </div>
+</footer>
+
+</html>
---
+++ User/guest/header.php
@@ -0,0 +1,32 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HBMS Room Services</title>
+  <!-- Bootstrap CDN -->
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css.css" rel="stylesheet">
+  
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand fw-bold" href="#" style="margin-left: 200px;">HBMS</a>
+    <div class="collapse navbar-collapse justify-content-end">
+      <ul class="navbar-nav">
+        <li class="nav-item" style="margin-right:100px;"><a class="nav-link fw-semibold" href="#">Home</a></li>
+        <li class="nav-item" style="margin-right:100px;"><a class="nav-link fw-semibold" href="#">Accommodation</a></li>
+        <li class="nav-item" style="margin-right:450px;"><a class="nav-link fw-semibold" href="#">Manage Profile</a></li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="margin-right: 100px;"><i class="bi bi-person-circle"></i> Moses Alfonso</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
